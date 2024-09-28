63e1b90a757af40ae62223a7f8a94605282df05a
Galleons2029 <Galleons777@gmail.com>
2024-09-28 15:16:29 +0800

    #release/alpha-0.02

--- app/db/models.py
+++ app/db/models.py
@@ -66,3 +66,74 @@ class StudentCollection(BaseModel):
 
 
 
+
+
+class JobInModel(BaseModel):
+    """
+    输入上下文以及需要解析的文本描述的容器。
+    """
+
+    #id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    #job_name: str = Field(...)
+    company_name: str = Field(...)
+    #position_name: str = Field(...)
+    companyIntro: str = Field(...)
+    positionIntro: str = Field(...)
+
+    model_config = ConfigDict(
+        populate_by_name=True,
+        arbitrary_types_allowed=True,
+        json_schema_extra={
+            "example": {
+                "job_name": "PHP工程师",
+                "company_name": "云烟科技有限公司",
+                "position_name": "数据科学",
+                "companyIntro": "wafjiawjfiojawiofjawjf",
+                "positionIntro": "dhaiwdhiawhfiawhi"
+            }
+        },
+    )
+
+
+class JobOutModel(BaseModel):
+    """
+    单个职位描述的容器。
+    """
+
+    # 职位模型的主键，存储为实例上的 `str`。
+    # 这将在发送到 MongoDB 时别名为 `_id`，
+    # 但在 API 请求和响应中提供为 `id`。
+    #id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    job_name: str = Field(...)
+    parent_category: str = Field(...)
+    second_category: str = Field(...)
+    cities: str = Field(...)
+    attribute: str = Field(...)
+    education: str = Field(...)
+    salary: str = Field(...)
+    about_major: str = Field(...)
+    number: int = Field(..., le=0)
+
+    duty: str = Field(...)
+    explain: str = Field(...)
+    requirements: str = Field(...)
+
+    model_config = ConfigDict(
+        populate_by_name=True,
+        arbitrary_types_allowed=True,
+        json_schema_extra={
+            "example": {
+                "job_name": "营养师",
+                "parent_category": "生物/制药/医疗/护理",
+                "second_category": "医院/医疗",
+                "cities": "长沙市",
+                "attribute": "校招",
+                "education": "本科及以上",
+                "salary": "长沙市",
+                "about_major": "经济学",
+                "number": 5,
+            }
+        },
+    )
+
+
